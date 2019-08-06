b4b5e00a82cfc6ab489458746bb6db9d25f4200c
League Student <league@iMac-14.attlocal.net>
2019-08-06 14:24:31 -0700

    day2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,19 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
+
+		Robot pewdiepie = new Robot();
+		pewdiepie.setSpeed(100);
+		pewdiepie.penDown();
 		
-		
+		for (int i = 0; i < 4; i++) {
+			pewdiepie.move(200);
+			pewdiepie.turn(90);
+		}
+
 	}
 }
